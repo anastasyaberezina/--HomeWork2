aa24dd8ca34f06c7cc30c7499f35830bd82d7b86
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-24 20:45:58 +0300

    дни недели: будний или выходной

---
+++ HomeWork2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Является ли день недели выходным?
+
+Console.WriteLine("Введите порядковое число дня недели: ");
+int x = int.Parse(Console.ReadLine());
+int[] array = {0, 1, 2, 3, 4, 5, 6, 7};
+int a = x;
+int index = array[a]-1;
+array[a] = array[x];
+
+if (array[x]<array[6])
+{
+    Console.WriteLine("Сегодня будний день(");
+}
+else
+{
+    Console.WriteLine("Сегодня ВЫХОДНОЙ!");
+}
